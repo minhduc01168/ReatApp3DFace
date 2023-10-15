a7c889c51a8eb9249256498a2eeccde5df244c8a
minhduc01168 <phucanhthien@gmail.com>
2023-10-15 10:52:03 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import { Canvas } from "@react-three/fiber";
+import { useGLTF, Stage, PresentationControls } from "@react-three/drei";
+
+function Model(props) {
+  const { scene } = useGLTF("/mesh0.glb");
+  return <primitive object={scene} {...props} />
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Canvas dpr={[1,2]} shadows camera={{ fov: 45 }} style={{"position": "absolute"}}>
+      <color attach="background" args={["#101010"]} />
+      <PresentationControls speed={1.5} global zoom={.5} polar={[-0.1, Math.PI / 4]}>
+        <Stage environment={"sunset"}>
+          <Model scale={0.01} />
+        </Stage>
+      </PresentationControls>
+    </Canvas>
   );
 }
 
 export default App;
+
+// import { Canvas } from '@react-three/fiber';
+// import { OrbitControls, useGLTF, Stage, PresentationControls } from '@react-three/drei';
+
+// function Model(props) {
+//   const { scene } = useGLTF('/mesh0.glb');
+
+//   return (
+//     <primitive object={scene} {...props}>
+//       <mesh 
+//         onClick={(e) => console.log('Clicked on mesh', e.object)}
+//       >
+//         <boxGeometry args={[1, 1, 1]} />
+//         <meshStandardMaterial color="hotpink" />
+//       </mesh>
+//     </primitive>
+//   );
+// }
+
+// function App() {
+//   return (
+//     <Canvas shadows dpr={[1, 2]} camera={{ fov: 45 }} style={{ position: 'absolute' }}>
+//       <color attach="background" args={['#101010']} />
+
+//       <OrbitControls 
+//         enableZoom={true}
+//         enablePan={true}
+//         enableRotate={true} // cho phép xoay 360 độ
+//       />
+
+//       <PresentationControls
+//         global
+//         rotation={[0, 0, 0]}
+//         polar={[-0.1, Math.PI / 4]}
+//         azimuth={[-1, 1]}  
+//       >
+
+//         <Stage environment={'sunset'}>
+//           <Model scale={0.01} />
+//         </Stage>
+        
+//       </PresentationControls>
+
+//     </Canvas>
+//   );
+// }
+
